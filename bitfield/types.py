0aca212679d3b97d55202b831e50b6259fd9f304
Taha Jahangir <mtjahangir@gmail.com>
2019-01-10 12:24:55 +0330

    Fix RemovedInDjango30Warning about using deprecated postgresql_psycopg2

--- bitfield/types.py
+++ bitfield/types.py
@@ -260,7 +260,7 @@ if django.VERSION[:2] >= (1, 8):
         pass
 
     try:
-        from django.db.backends.postgresql_psycopg2.base import Database
+        from django.db.backends.postgresql.base import Database
         Database.extensions.register_adapter(Bit, lambda x: Database.extensions.AsIs(int(x)))
         Database.extensions.register_adapter(BitHandler, lambda x: Database.extensions.AsIs(int(x)))
     except ImproperlyConfigured:
